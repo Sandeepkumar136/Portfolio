b05822fdc07d9d6206a6e324272156a1b32ffd58
Sandeep_kumar136 <Ksundip750#gmail.com>
2024-05-20 18:45:39 +0530

    created havebar

---
+++ script.js
@@ -0,0 +1,42 @@
+// toggle function
+const openBUtton=document.getElementById('toggle_function');
+const sidebar=document.querySelector('.sidebar');
+const closeBUtton=document.getElementById('close-button');
+const navbar=document.querySelector('.nav');
+
+
+openBUtton.addEventListener('click', ()=>{
+    sidebar.classList.add('show-sidebar');
+    navbar.classList.add('hide-nav-bar');
+});
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    sidebar.classList.remove('show-sidebar');
+
+    let wordIndex=0;
+    let chartIndex=0;
+    let isDeleting=false;
+    const words=["Frontend Developer", "Backend Developer", "Fullstack Developer"];
+    const texttyper=document.querySelector('.typewritter');
+    function type(){
+        const currentword=words[wordIndex];
+        const currentText=isDeleting ? currentword.substring(0, chartIndex --):currentword.substring(0, chartIndex++);
+
+        texttyper.textContent=currentText;
+
+        if(!isDeleting && chartIndex===currentword.length){
+            setTimeout(()=>(isDeleting=true), 1000);
+        }else if(isDeleting && chartIndex===0){
+            isDeleting=false;
+            wordIndex=(wordIndex +1)%words.length;
+        }
+        const typingspeend=isDeleting ?50:100;
+        setTimeout(type, typingspeend);
+    }
+    type();
+});
+
+closeBUtton.addEventListener('click', ()=>{
+    sidebar.classList.remove('show-sidebar');
+    navbar.classList.remove('hide-nav-bar');
+})
